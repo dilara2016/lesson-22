e1d2b6d3e88a43158968dab1cd82c630eac67939
dilara2016 <m.123.hunain@gmail.com>
2024-12-15 13:44:03 +0100

    Update interssection.py

--- interssection.py
+++ interssection.py
@@ -1,8 +1,10 @@
 setx = {"green", "blue"}
 sety ={"blue", "yellow"}
+seta ={"blue, brown"}
 print("original set elements:")
 print(setx)
 print(sety)
+print(seta)
 print("\nIntersection of 2 said sets:")
 setz = setx.intersection(sety)
 print(setz)
