dd9ebbbb65bbffbf2627b0981194b91076a16d5b
dilara2016 <m.123.hunain@gmail.com>
2024-12-15 10:32:05 +0100

    setoperaition

---
+++ set operations.py
@@ -0,0 +1,13 @@
+my_set = {1,2,3}
+print(my_set)
+ny_set = {1,0, "Hello", (1,2,3)}
+my_set ={1,2,3,4,3,2}
+print(my_set)
+my_set = set[1,2,3,2]
+print(my_set,"\n")
+num_set = set([0,1,3,4,5])
+print("Original set:")
+print(num_set)
+num_set.pop()
+print("after removing the firs element from the said set")
+print(num_set,"\n")
